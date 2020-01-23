0876e173581eac3c967e092d40cf74438471e15b
Danny Soto <87dsoto@gmail.com>
2020-01-23 22:32:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |key, val|
-      puts "  #{key.capitalize}: #{val.join(", ")}"
+      puts "  #{key.split("_").capitalize.join(" ")}: #{val.join(", ")}"
     end
   end
 end
