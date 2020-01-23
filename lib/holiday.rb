18951e1cff74900eb4f0f573cd9d229aaa90fed6
Danny Soto <87dsoto@gmail.com>
2020-01-23 22:29:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  
+  holiday_hash.each do |season, holiday|
+    puts "#{season}"
+    holiday.each do |key, val|
+      puts "#{key}: #{val.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
